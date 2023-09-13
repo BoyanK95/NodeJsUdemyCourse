22518beada4af37cb1be30f576a10b8ee748fa3e
BoyanK95 <boyan_k_95@abv.bg>
2023-09-13 07:29:27 +0300

    feat: create dummy backedn page

--- Section3/app.js
+++ Section3/app.js
@@ -2,8 +2,15 @@ const http = require('http')
 
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method, req.headers);
-    console.log('req obj!!');
-    console.log(req);
+    // console.log('req obj!!');
+    // console.log(req);
+    res.setHeader('Content-Type', 'text/html')
+    res.write('<html>')
+    res.write('<head><title>My First Backend Server Page</title></head>')
+    res.write('<body><h1>Hello from Node!</h1></body>')
+    res.write('</html>')
+
+    res.end()
 })
 
 server.listen(3000)
