cd6ddacc646f53eddd587b0b3436782188ccf446
BoyanK95 <boyan_k_95@abv.bg>
2023-09-13 09:03:28 +0300

    fix: fs.writeFile async way

--- Section3/app.js
+++ Section3/app.js
@@ -1,5 +1,5 @@
 const http = require("http");
-const fs = require('fs')
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   // console.log(req.url, req.method, req.headers);
@@ -7,7 +7,7 @@ const server = http.createServer((req, res) => {
   // console.log(req);
 
   const url = req.url;
-  const method = req.method
+  const method = req.method;
 
   if (url === "/") {
     res.write("<html>");
@@ -16,25 +16,27 @@ const server = http.createServer((req, res) => {
       '<body><form action="message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
     );
     res.write("</html>");
-    return res.end()
+    return res.end();
   }
 
-  if (url === '/message' && method === 'POST') {
-    const body = []
-    req.on('data', (chunk) => {
-        console.log(chunk);
-        body.push(chunk)
-    })
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
 
-    req.on('end', ()=> {
-        const parseBody = Buffer.concat(body).toString()
-        console.log(parseBody);
-    })
+    return req.on("end", () => {
+      const parseBody = Buffer.concat(body).toString();
+      console.log(parseBody);
+      const message = parseBody.split("=")[1];
 
-    fs.writeFileSync('message.txt', "DummyData")
-    res.statusCode = 302
-    res.setHeader('Location', '/')
-    return res.end()
+      fs.writeFile("message.txt", message, (err) => {
+        res.statusCode = 302;
+        res.setHeader("Location", "/");
+        return res.end();
+      });
+    });
   }
 
   res.setHeader("Content-Type", "text/html");
