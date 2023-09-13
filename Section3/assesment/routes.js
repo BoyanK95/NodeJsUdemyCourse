ac3d01aece7ee45379af5aa11e7bf9e4ca5133f3
BoyanK95 <boyan_k_95@abv.bg>
2023-09-13 23:09:28 +0300

    fix: add status code and redirection

--- Section3/assesment/routes.js
+++ Section3/assesment/routes.js
@@ -18,19 +18,23 @@ const routesHandler = (req, res) => {
       body.push(chunk);
     });
 
-    return req.on("end", () => {
+    req.on("end", () => {
       const parsedBody = Buffer.concat(body).toString();
       console.log(parsedBody);
       const username = parsedBody.split("=")[1];
       console.log(username);
     });
+
+    res.statusCode = 302;
+    res.setHeader("Location", '/');
+    res.end()
   }
 
-  if (url === '/users') {
+  if (url === "/users") {
     res.write("<html>");
     res.write("<head><title>Hello Exam</title></head>");
     res.write(
-      '<body><ul><li>User 1</li><li>User 2</li><li>User 3</li></ul></body>'
+      "<body><ul><li>User 1</li><li>User 2</li><li>User 3</li></ul></body>"
     );
     res.write("</html>");
     return res.end();
