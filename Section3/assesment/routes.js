e5fb086bf815d6b0a95f423704ce219487a3ca5b
BoyanK95 <boyan_k_95@abv.bg>
2023-09-13 23:05:08 +0300

    feat: finish second assingment server

---
+++ Section3/assesment/routes.js
@@ -0,0 +1,40 @@
+const routesHandler = (req, res) => {
+  const url = req.url;
+  const method = req.method;
+
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Hello Exam</title></head>");
+    res.write(
+      '<body><h1>Hello there!</h1><h3>Please enter a user</h3><form action="/create-user" method="POST"><input type="text" name="create-user"><button type="submit">Send</button></form></body>'
+    );
+    res.write("</html>");
+    return res.end();
+  }
+
+  if (url === "/create-user" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+
+    return req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      console.log(parsedBody);
+      const username = parsedBody.split("=")[1];
+      console.log(username);
+    });
+  }
+
+  if (url === '/users') {
+    res.write("<html>");
+    res.write("<head><title>Hello Exam</title></head>");
+    res.write(
+      '<body><ul><li>User 1</li><li>User 2</li><li>User 3</li></ul></body>'
+    );
+    res.write("</html>");
+    return res.end();
+  }
+};
+
+module.exports = routesHandler;
