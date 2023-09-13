fbd800191ed3f9a32e7316b67205d112365d1ea8
BoyanK95 <boyan_k_95@abv.bg>
2023-09-13 07:36:09 +0300

    feat: create dummy form for root page

--- Section3/app.js
+++ Section3/app.js
@@ -1,16 +1,28 @@
-const http = require('http')
+const http = require("http");
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
-    // console.log('req obj!!');
-    // console.log(req);
-    res.setHeader('Content-Type', 'text/html')
-    res.write('<html>')
-    res.write('<head><title>My First Backend Server Page</title></head>')
-    res.write('<body><h1>Hello from Node!</h1></body>')
-    res.write('</html>')
+  // console.log(req.url, req.method, req.headers);
+  // console.log('req obj!!');
+  // console.log(req);
 
-    res.end()
-})
+  const url = req.url;
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Enter Message</title></head>");
+    res.write(
+      '<body><form action="message" method="POST"><input type="text"><button type="submit">Send</button></form></body>'
+    );
+    res.write("</html>");
+    return res.end()
+  }
 
-server.listen(3000)
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My First Backend Server Page</title></head>");
+  res.write("<body><h1>Hello from Node!</h1></body>");
+  res.write("</html>");
+
+  res.end();
+});
+
+server.listen(3000);
