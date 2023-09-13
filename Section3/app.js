2c8c2bdb4b17d815bb74ba9c69858ad5215ecf58
BoyanK95 <boyan_k_95@abv.bg>
2023-09-13 07:45:08 +0300

    feat: create POST route

--- Section3/app.js
+++ Section3/app.js
@@ -1,4 +1,5 @@
 const http = require("http");
+const fs = require('fs')
 
 const server = http.createServer((req, res) => {
   // console.log(req.url, req.method, req.headers);
@@ -6,6 +7,8 @@ const server = http.createServer((req, res) => {
   // console.log(req);
 
   const url = req.url;
+  const method = req.method
+
   if (url === "/") {
     res.write("<html>");
     res.write("<head><title>Enter Message</title></head>");
@@ -13,6 +16,13 @@ const server = http.createServer((req, res) => {
       '<body><form action="message" method="POST"><input type="text"><button type="submit">Send</button></form></body>'
     );
     res.write("</html>");
+    return res.end()
+  }
+
+  if (url === '/message' && method === 'POST') {
+    fs.writeFileSync('message.txt', "DummyData")
+    res.statusCode = 302
+    res.setHeader('Location', '/')
     return res.end()
   }
 
