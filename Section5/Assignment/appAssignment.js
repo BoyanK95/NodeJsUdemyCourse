e5ad55a3ace1ec0db3f4eadb8a2581872d2fa3ad
BoyanK95 <boyan_k_95@abv.bg>
2023-09-14 00:15:55 +0300

    feat: finish assignment

---
+++ Section5/Assignment/appAssignment.js
@@ -0,0 +1,18 @@
+const http = require("http");
+const express = require("express");
+
+const app = express();
+
+app.use("/users", (req, res, next) => {
+  res.send("<h1>The Add Users Page</h1>");
+
+  next();
+});
+
+app.use("/", (req, res, next) => {
+  res.send("<h1>Home Page</h1>");
+
+  next();
+});
+
+app.listen(3001);
