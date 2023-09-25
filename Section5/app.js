41d2827ee06a642cdfe4c0bac6d0ec9a901952ec
BoyanK95 <boyan_k_95@abv.bg>
2023-09-25 10:05:22 +0300

    feat: used body-parser to parse request

--- Section5/app.js
+++ Section5/app.js
@@ -1,10 +1,15 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: false}))
+
 app.use("/add-product", (req, res, next) => {
-    res.send("<h1>The Add product page</h1>");
-  });
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Send</button></form>'
+  );
+});
 
 app.use("/", (req, res, next) => {
   res.send("<h1>Hello from Express!</h1>");
