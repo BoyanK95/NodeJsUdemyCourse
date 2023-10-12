34c1dacf5bb1c9bdea35002a9c1dce6c172ffa66
BoyanK95 <boyan_k_95@abv.bg>
2023-10-12 21:48:16 +0300

    fix: connected User with a Product using userId

--- Section12MongoDB/controllers/admin.js
+++ Section12MongoDB/controllers/admin.js
@@ -19,8 +19,8 @@ exports.postAddProduct = (req, res, next) => {
     price,
     description,
     imageUrl,
-    null
-    // req.user._id
+    null,
+    req.user._id
   );
   console.log(product);
   product
